963409c53ec08b64023cdc21c4607bd33ee9cec4
YKlimchuk_li <yklimchuk_li@inno.tech>
2025-01-21 16:05:52 +0300

    fixed mongoautocofig

--- server/src/main/java/com/hydroyura/prodms/warehouse/server/Server.java
+++ server/src/main/java/com/hydroyura/prodms/warehouse/server/Server.java
@@ -3,12 +3,15 @@ package com.hydroyura.prodms.warehouse.server;
 import com.hydroyura.prodms.warehouse.server.props.MongoProps;
 import com.hydroyura.prodms.warehouse.server.props.ValidationGetAllMaterialParamsProps;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
 @EnableConfigurationProperties({MongoProps.class, ValidationGetAllMaterialParamsProps.class})
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class Server {
 
     public static void main(String[] args) {
